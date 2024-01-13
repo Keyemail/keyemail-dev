af21ac2a2e6f70a12a6e36b4e16718ef68b11d84
Keyemail <67425124+Keyemail@users.noreply.github.com>
2024-01-13 11:23:42 -0800

    compressed images

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -2,41 +2,41 @@ export default [
   {
     name: "2 cats",
     id: "1",
-    path: "gallery/IMG_0391.png"
+    path: "gallery/2_cats.jpg"
   },
   {
     name: "Friends Cat",
     id: "2",
-    path: "gallery/IMG_2381.png"
+    path: "gallery/friend_cat.jpg"
   },
   {
     name: "Miku Poster",
     id: "3",
-    path: "gallery/IMG_2798.png"
+    path: "gallery/miku_poster.jpg"
   },
   {
     name: "Networking",
     id: "4",
-    path: "gallery/IMG_3166.png"
+    path: "gallery/networking.jpg"
   },
   {
     name: "Another Friends Cat",
     id: "5",
-    path: "gallery/IMG_3898.png"
+    path: "gallery/another_friend_cat.jpg"
   },
   {
     name: "PC Stickers",
     id: "6",
-    path: "gallery/IMG_4809.png"
+    path: "gallery/pc_case_stickers.jpg"
   },
   {
     name: "Movie Theater Picture 1",
     id: "7",
-    path: "gallery/IMG_4810.png"
+    path: "gallery/movie_picture_1.jpg"
   },
   {
     name: "Movie Theater Picture 2",
     id: "8",
-    path: "gallery/IMG_4812.png"
+    path: "gallery/movie_picture_2.jpg"
   }
 ]
